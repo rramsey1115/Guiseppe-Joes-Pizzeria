a765b99568b4bc6c6bb6731359ea6ab51a9d756d
Ryan Ramsey <rramsey1115@gmail.com>
2024-01-09 15:53:10 -0600

    updated DTOs

--- Models/DTOs/OrderDTO.cs
+++ Models/DTOs/OrderDTO.cs
@@ -5,7 +5,7 @@ public class OrderDTO
     public int Id { get; set; }
     public int EmployeeId { get; set; }
     [ForeignKey("EmployeeId")]
-    public Employee Employee { get; set; }
+    public EmployeeDTO Employee { get; set; }
     public DateTime PlacedOnDate { get; set; }
     public bool Delivery { get; set; }
     public decimal Tip { get; set; }
@@ -13,8 +13,8 @@ public class OrderDTO
     public string? DeliveryAddress { get; set; }
     public int DriverId { get; set; }
     [ForeignKey("DriverId")]
-    public Employee? Driver { get; set; }
-    public List<Pizza> OrderPizzas { get; set; }
+    public EmployeeDTO? Driver { get; set; }
+    public List<PizzaDTO> OrderPizzas { get; set; }
     public decimal TotalCost { 
         get
         {
