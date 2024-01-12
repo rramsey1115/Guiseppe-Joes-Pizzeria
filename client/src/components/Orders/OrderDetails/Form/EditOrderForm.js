723b5ebb550516e41357e276dccd320a161a9757
Ryan Ramsey <rramsey1115@gmail.com>
2024-01-11 22:16:04 -0600

    delivery radios work

--- client/src/components/Orders/OrderDetails/Form/EditOrderForm.js
+++ client/src/components/Orders/OrderDetails/Form/EditOrderForm.js
@@ -1,10 +1,17 @@
 import RingLoader from "react-spinners/RingLoader";
 import "./EditOrderForm.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const EditOrderForm = ({ setFormOpen, setOrder, order }) => {
-    const [delivery, setDelivery] = useState(order?.delivery);
+    const [updatedObj, setUpdatedObj] = useState({
+        delivery: order.delivery,
+    });
 
+    useEffect(() => { 
+        if(order.delivery) { updatedObj.delivery = order.delivery}
+    }, [order]);
+
+    
     return !order 
     ? <div className="spinner-div">
         <RingLoader
@@ -12,25 +19,51 @@ export const EditOrderForm = ({ setFormOpen, setOrder, order }) => {
             loading
             size={80}
             speedMultiplier={2}
-        />
+            />
     </div>
     : (
-    <form autoComplete="true" className="edit-details-form">
+        <form autoComplete="true" className="edit-details-form">
+        {console.log('order', order)}
+        {console.log('del', order.delivery)}
         <fieldset className="form-control">
                 <h5>Type</h5>
                 <div className="radios">
-                    <input 
-                        name="type" 
-                        type="radio" 
-                        defaultChecked={order.delivery ? true : false }
-                        onSelect={() => setDelivery(true)}
-                    />Delivery
-                    <input 
-                        defaultChecked={order.delivery ? false : true }
-                        name="type" 
-                        type="radio"
-                        onSelect={() => setDelivery(false)}
-                    />Dine-In/Take-Out
+                    {order.delivery===true 
+                        ? <div>
+                            <input 
+                                name="type" 
+                                type="radio" 
+                                defaultChecked
+                                onClick={() => {
+                                    const copy = {...updatedObj}
+                                    copy.delivery = true
+                                    setUpdatedObj(copy)
+                                }}
+                            />Delivery
+                        </div>
+                        : <div>
+                            <input 
+                                name="type" 
+                                type="radio" 
+                                onClick={() => {
+                                    const copy = {...updatedObj}
+                                    copy.delivery = true;
+                                    setUpdatedObj(copy);
+                                }}
+                            />Delivery
+                        </div>
+                    } 
+                    <div>
+                        <input 
+                            name="type" 
+                            type="radio"
+                            onClick={() => {
+                                const copy = {...updatedObj}
+                                copy.delivery = false;
+                                setUpdatedObj(copy);
+                            }}
+                        />Dine-In/Take-Out
+                    </div>
                 </div>
         </fieldset>
         <fieldset className="form-control">
